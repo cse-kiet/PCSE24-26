53abf90932a197397b16a6425c897c78cab2385c
shorya <shoryagarg69@gmail.com>
2021-10-04 22:13:00 +0530

    new activity added rajor pay

--- app/src/main/java/com/example/foodyhomeSS/Payment_Activity.java
+++ app/src/main/java/com/example/foodyhomeSS/Payment_Activity.java
@@ -2,13 +2,25 @@ package com.example.foodyhomeSS;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Payment_Activity extends AppCompatActivity {
+    Button Payment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_);
+        Payment=findViewById(R.id.PaymentMethod);
+        Payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Payment_Activity.this , Razorpay.class);
+                startActivity(i);
+            }
+        });
     }
 }
